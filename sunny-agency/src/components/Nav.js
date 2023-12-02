df69b5e973b29d5e5f690850d1cba482c2bab71f
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-12-02 16:33:59 +0000

    return of the coding codes

---
+++ sunny-agency/src/components/Nav.js
@@ -0,0 +1,16 @@
+export default function Topnav(){
+    <div className="top-nav-container">
+        <img src="" alt="" />
+        <nav>
+            <img src="" alt="" />
+            <div className="links-wrap">
+                <ul>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Project</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+}
